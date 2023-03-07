c1c2d1def535f1f251652005d41900634617f2b7
Ina Araujo <inaiaraaraujo25@hotmail.com>
2023-03-07 15:01:46 -0300

    Iniciando API

---
+++ All-for-Food/src/main/java/com/example/demo/AllForFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllForFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllForFoodApplication.class, args);
+	}
+
+}
